9125b3b2f995835a45c9fa640dc40d23f3417f32
Ekky Armandi <ekkyarmandi@gmail.com>
2024-12-19 09:18:07 +0700

    fix: adding error catcher in spider level

--- crawler/real_estate_scraper/spiders/a4zida.py
+++ crawler/real_estate_scraper/spiders/a4zida.py
@@ -4,9 +4,10 @@ import math
 import re
 import uuid
 import jmespath
-import json
+import traceback
 
-from real_estate_scraper.func import raw_json_formatter
+from real_estate_scraper.database import get_db
+from models.error import Error
 
 
 class A4zidaSpider(scrapy.Spider):
@@ -46,126 +47,143 @@ class A4zidaSpider(scrapy.Spider):
                 yield response.follow(next_url)
 
     def parse_detail(self, response):
-        elapsed_time = response.meta.get("download_latency")
-        # find property data
-        data = self.find_property_data(response)
-        lonlat = self.find_longitude_latitude(response)
-        images = self.get_images(data)
-        # assign data
-        phonenumber = jmespath.search("author.phones[0].national", data)
-        seller_name = jmespath.search("author.fullName", data)
-        # missing value
-        incomplete_pn = response.css("button::text").re_first("^0[0-9 ]+")
-        descriptions = response.css(
-            "main > section div[test-data=rich-text-description] ::Text"
-        ).getall()
-        descriptions = list(map(str.strip, descriptions))
-        # find address
-        address = response.css("h1 + div span::text").get()
-        city = jmespath.search("placeMetaData[0].title", data)
-        municipality = jmespath.search("placeMetaData[1].title", data)
-        micro_location = jmespath.search("placeMetaData[2].title", data)
-        x, y, z = None, None, None
-        if address:
-            address = address.split(",")
-            address = list(map(str.strip, address))
-            if len(address) > 2:
-                z, y, x = address[:3]
-        page = dict(
-            title=response.css("h1 ::Text").get(),
-            price=response.css("p[test-data=ad-price] ::Text").re_first(r"[0-9.,]+"),
-            descriptions="\n\n".join(descriptions),
-            property=dict(
-                size_m2=response.css(
-                    "strong:contains('m²'),strong:contains('m2')"
-                ).re_first(r"[0-9.,]+"),
-                rooms=response.css(
-                    "strong:contains('rooms'),strong:contains('sobe')"
-                ).re_first(r"[0-9.,]+"),
-                floor_number=response.css("strong:contains('sprata')").re_first(
-                    r"([0-9.,]+)/"
+        try:
+            elapsed_time = response.meta.get("download_latency")
+            # find property data
+            data = self.find_property_data(response)
+            lonlat = self.find_longitude_latitude(response)
+            images = self.get_images(data)
+            # assign data
+            phonenumber = jmespath.search("author.phones[0].national", data)
+            seller_name = jmespath.search("author.fullName", data)
+            # missing value
+            incomplete_pn = response.css("button::text").re_first("^0[0-9 ]+")
+            descriptions = response.css(
+                "main > section div[test-data=rich-text-description] ::Text"
+            ).getall()
+            descriptions = list(map(str.strip, descriptions))
+            # find address
+            address = response.css("h1 + div span::text").get()
+            city = jmespath.search("placeMetaData[0].title", data)
+            municipality = jmespath.search("placeMetaData[1].title", data)
+            micro_location = jmespath.search("placeMetaData[2].title", data)
+            x, y, z = None, None, None
+            if address:
+                address = address.split(",")
+                address = list(map(str.strip, address))
+                if len(address) > 2:
+                    z, y, x = address[:3]
+            page = dict(
+                title=response.css("h1 ::Text").get(),
+                price=response.css("p[test-data=ad-price] ::Text").re_first(
+                    r"[0-9.,]+"
                 ),
-                total_floors=response.css("strong:contains('sprata')").re_first(
-                    r"/([0-9.,]+)"
+                descriptions="\n\n".join(descriptions),
+                property=dict(
+                    size_m2=response.css(
+                        "strong:contains('m²'),strong:contains('m2')"
+                    ).re_first(r"[0-9.,]+"),
+                    rooms=response.css(
+                        "strong:contains('rooms'),strong:contains('sobe')"
+                    ).re_first(r"[0-9.,]+"),
+                    floor_number=response.css("strong:contains('sprata')").re_first(
+                        r"([0-9.,]+)/"
+                    ),
+                    total_floors=response.css("strong:contains('sprata')").re_first(
+                        r"/([0-9.,]+)"
+                    ),
                 ),
-            ),
-            seller=dict(
-                name=response.css("section[test-data=author-info] span::text").get(),
-                phonenumber=response.css(
-                    f"script:contains('{incomplete_pn}')::text"
-                ).re_first(r"\d{3}\s+\d{7}"),
-            ),
-            address=dict(
-                city=z,
-                municipality=y,
-                micro_location=x,
-            ),
-        )
-        yield {
-            "listing_id": str(uuid.uuid4()),
-            "elapsed_time": elapsed_time,
-            "source_id": data.get("id"),
-            "title": data.get("title", page["title"]),
-            "short_description": data.get("humanReadableDescription"),
-            "detail_description": data.get("desc", page["descriptions"]),
-            "price": data.get("price", page["price"]),
-            "price_currency": "EUR",  # TODO: find in HTML
-            "status": "active",
-            "valid_from": None,  # COMMENT: not sure which data point to look
-            "valid_to": None,  # COMMENT: not sure which data point to look
-            "total_views": 0,
-            "url": response.url,
-            "raw_data": {
-                "html": response.text,
-                "data": {
-                    "property_data": data,
-                    "geolocation_data": lonlat,
+                seller=dict(
+                    name=response.css(
+                        "section[test-data=author-info] span::text"
+                    ).get(),
+                    phonenumber=response.css(
+                        f"script:contains('{incomplete_pn}')::text"
+                    ).re_first(r"\d{3}\s+\d{7}"),
+                ),
+                address=dict(
+                    city=z,
+                    municipality=y,
+                    micro_location=x,
+                ),
+            )
+            yield {
+                "listing_id": str(uuid.uuid4()),
+                "elapsed_time": elapsed_time,
+                "source_id": data.get("id"),
+                "title": data.get("title", page["title"]),
+                "short_description": data.get("humanReadableDescription"),
+                "detail_description": data.get("desc", page["descriptions"]),
+                "price": data.get("price", page["price"]),
+                "price_currency": "EUR",  # TODO: find in HTML
+                "status": "active",
+                "valid_from": None,  # COMMENT: not sure which data point to look
+                "valid_to": None,  # COMMENT: not sure which data point to look
+                "total_views": 0,
+                "url": response.url,
+                "raw_data": {
+                    "html": response.text,
+                    "data": {
+                        "property_data": data,
+                        "geolocation_data": lonlat,
+                    },
                 },
-            },
-            ## additional data
-            "property": {
-                "property_type": data.get("type"),
-                "building_type": data.get("category"),
-                "size_m2": data.get("m2", page["property"]["size_m2"]),
-                "floor_number": data.get(
-                    "redactedFloor", page["property"]["floor_number"]
-                ),
-                "total_floors": data.get(
-                    "redactedTotalFloors", page["property"]["total_floors"]
-                ),
-                "rooms": page["property"]["rooms"],  # TODO: find in HTML
-                "property_state": data.get("state"),
-            },
-            "address": {
-                "city": city if city else page["address"]["city"],
-                "municipality": (
-                    municipality if municipality else page["address"]["municipality"]
-                ),
-                "micro_location": (
-                    micro_location
-                    if micro_location
-                    else page["address"]["micro_location"]
-                ),
-                "latitude": lonlat.get("latitude"),
-                "longitude": lonlat.get("longitude"),
-            },
-            "source": {
-                "id": str(uuid.uuid4()),
-                "name": "4zida.rs",
-                "base_url": "https://www.4zida.rs",
-            },
-            "seller": {
-                "source_seller_id": jmespath.search("author.id", data),
-                "name": seller_name if seller_name else page["seller"]["name"],
-                "seller_type": "agency" if data.get("advertiserType") else "other",
-                "primary_phone": (
-                    phonenumber if phonenumber else page["seller"]["phonenumber"]
-                ),
-                "primary_email": jmespath.search("author.agency.email", data),
-                "website": None,
-            },
-            "images": images,
-        }
+                ## additional data
+                "property": {
+                    "property_type": data.get("type"),
+                    "building_type": data.get("category"),
+                    "size_m2": data.get("m2", page["property"]["size_m2"]),
+                    "floor_number": data.get(
+                        "redactedFloor", page["property"]["floor_number"]
+                    ),
+                    "total_floors": data.get(
+                        "redactedTotalFloors", page["property"]["total_floors"]
+                    ),
+                    "rooms": page["property"]["rooms"],  # TODO: find in HTML
+                    "property_state": data.get("state"),
+                },
+                "address": {
+                    "city": city if city else page["address"]["city"],
+                    "municipality": (
+                        municipality
+                        if municipality
+                        else page["address"]["municipality"]
+                    ),
+                    "micro_location": (
+                        micro_location
+                        if micro_location
+                        else page["address"]["micro_location"]
+                    ),
+                    "latitude": lonlat.get("latitude"),
+                    "longitude": lonlat.get("longitude"),
+                },
+                "source": {
+                    "id": str(uuid.uuid4()),
+                    "name": "4zida.rs",
+                    "base_url": "https://www.4zida.rs",
+                },
+                "seller": {
+                    "source_seller_id": jmespath.search("author.id", data),
+                    "name": seller_name if seller_name else page["seller"]["name"],
+                    "seller_type": "agency" if data.get("advertiserType") else "other",
+                    "primary_phone": (
+                        phonenumber if phonenumber else page["seller"]["phonenumber"]
+                    ),
+                    "primary_email": jmespath.search("author.agency.email", data),
+                    "website": None,
+                },
+                "images": images,
+            }
+        except Exception as e:
+            db = next(get_db())
+            error_data = Error(
+                url=response.url,
+                error_type="Spider",
+                error_message=str(e),
+                error_traceback=traceback.format_exc(),
+            )
+            db.add(error_data)
+            db.commit()
 
     def find_property_data(self, response):
         script = response.css("script:contains('superIndividual')::Text").get()
--- crawler/real_estate_scraper/spiders/halooglasi.py
+++ crawler/real_estate_scraper/spiders/halooglasi.py
@@ -5,8 +5,11 @@ import re
 import uuid
 import jmespath
 import json
+import traceback
 
+from real_estate_scraper.database import get_db
 from real_estate_scraper.decorators import json_finder
+from models.error import Error
 
 
 class HaloOglasiNekretnineSpider(scrapy.Spider):
@@ -83,124 +86,141 @@ class HaloOglasiNekretnineSpider(scrapy.Spider):
         )
 
     def parse_detail(self, response):
-        elapsed_time = response.meta.get("elapsed_time")
-        short_description = response.meta.get("short_description")
-        source_url = response.meta.get("url")
-        response_text = response.meta.get("response_text")
-        it_has_numbers = lambda x: re.search(r"\d{3}", x)
+        try:
+            elapsed_time = response.meta.get("elapsed_time")
+            short_description = response.meta.get("short_description")
+            source_url = response.meta.get("url")
+            response_text = response.meta.get("response_text")
+            it_has_numbers = lambda x: re.search(r"\d{3}", x)
 
-        data = str(response.json())
-        phonenumber = re.findall(r">(.*?)<", data)
-        phonenumber = list(filter(it_has_numbers, phonenumber))
-        phonenumber = ", ".join(phonenumber)
+            data = str(response.json())
+            phonenumber = re.findall(r">(.*?)<", data)
+            phonenumber = list(filter(it_has_numbers, phonenumber))
+            phonenumber = ", ".join(phonenumber)
 
-        property_data = response.meta["property_data"]
-        agency_data = response.meta["agency_data"]
-        geolocation = property_data["GeoLocationRPT"]
-        root_url = "https://img.halooglasi.com"
-        image_urls = list(map(lambda x: root_url + x, property_data.get("ImageURLs")))
+            property_data = response.meta["property_data"]
+            agency_data = response.meta["agency_data"]
+            geolocation = property_data["GeoLocationRPT"]
+            root_url = "https://img.halooglasi.com"
+            image_urls = list(
+                map(lambda x: root_url + x, property_data.get("ImageURLs"))
+            )
 
-        yield {
-            "listing_id": str(uuid.uuid4()),
-            "elapsed_time": elapsed_time,
-            "source_id": property_data.get("Id"),
-            "title": property_data.get("Title"),
-            # "source_internal_id": None,
-            "short_description": short_description,  # get it from property card
-            "detail_description": property_data.get("TextHtml"),
-            "price": jmespath.search("OtherFields.cena_d", property_data),
-            "price_currency": jmespath.search(
-                "OtherFields.cena_d_unit_s", property_data
-            ),
-            "status": "active",  # if the property is not listed anymore put it as removed
-            "valid_from": property_data.get("ValidFrom"),
-            "valid_to": property_data.get("ValidTo"),
-            "total_views": property_data.get("TotalViews"),
-            # "agency_fee": None,
-            # "agency_fee_unit": property_data["OtherFields"][
-            #     "agencijska_sifra_oglasa_s"
-            # ],
-            "url": source_url,
-            "raw_data": {
-                "html": response_text,
-                "data": {
-                    "QuidditaEnvironmyent.CurrentClassified": property_data,
-                    "QuidditaEnvironment.CurrentContactData": agency_data,
-                },
-            },
-            ## additional data
-            "property": {
-                "property_type": jmespath.search(
-                    "OtherFields.tip_nekretnine_s", property_data
+            yield {
+                "listing_id": str(uuid.uuid4()),
+                "elapsed_time": elapsed_time,
+                "source_id": property_data.get("Id"),
+                "title": property_data.get("Title"),
+                # "source_internal_id": None,
+                "short_description": short_description,  # get it from property card
+                "detail_description": property_data.get("TextHtml"),
+                "price": jmespath.search("OtherFields.cena_d", property_data),
+                "price_currency": jmespath.search(
+                    "OtherFields.cena_d_unit_s", property_data
                 ),
-                "building_type": jmespath.search(
-                    "OtherFields.tip_objekta_s", property_data
-                ),
-                "size_m2": jmespath.search("OtherFields.kvadratura_d", property_data),
-                "floor_number": jmespath.search("OtherFields.sprat_s", property_data),
-                # COMMENT: sometime it's not available (sprat_od_s)
-                "total_floors": jmespath.search(
-                    "OtherFields.sprat_od_s", property_data
-                ),
-                "rooms": jmespath.search("OtherFields.broj_soba_s", property_data),
-                # COMMENT: sometime it's not available (stanje_objekta_s)
-                "property_state": jmespath.search(
-                    "OtherFields.stanje_objekta_s", property_data
-                ),
-                # "heating_type": jmespath.search("OtherFields.grejanje_s", property_data),
-                # "orientation": None,
-                # Note: Skip for the next iterations
-                # "has": [
-                #     # QUESTIONS: how I can find this data?
-                #     # ANSWER: OtherFields.ostalo_ss
-                #     {
-                #         "id": str(uuid.uuid4()),
-                #         # "feature_type": None,
-                #         "feature_value": None,
-                #         "created_at": now,
-                #     }
+                "status": "active",  # if the property is not listed anymore put it as removed
+                "valid_from": property_data.get("ValidFrom"),
+                "valid_to": property_data.get("ValidTo"),
+                "total_views": property_data.get("TotalViews"),
+                # "agency_fee": None,
+                # "agency_fee_unit": property_data["OtherFields"][
+                #     "agencijska_sifra_oglasa_s"
                 # ],
-            },
-            "address": {
-                "city": jmespath.search("OtherFields.grad_s", property_data),
-                "municipality": jmespath.search(
-                    "OtherFields.lokacija_s", property_data
-                ),
-                "micro_location": jmespath.search(
-                    "OtherFields.mikrolokacija_s", property_data
-                ),
-                "latitude": float(geolocation.split(",")[0]),
-                "longitude": float(geolocation.split(",")[1]),
-            },
-            "source": {
-                "id": str(uuid.uuid4()),
-                "name": "Halo Oglasi Nekretnine",
-                "base_url": "https://www.halooglasi.com",
-            },
-            "seller": {
-                "source_seller_id": property_data.get("AdvertiserId"),
-                "name": jmespath.search("Advertiser.DisplayName", agency_data),
-                # COMMENT: if there's number in register put seller type as 'agency'
-                "seller_type": (
-                    "agency" if agency_data.get("NumberInRegister") else None
-                ),
-                # "license_id": None,
-                # "tax_id": None,
-                "primary_phone": phonenumber,
-                "primary_email": None,
-                "website": agency_data["WebAddress"],
-                # "verified": None,
-                # "rating": None,
-                # "total_listings": None,
-                # "active_since": None,
-            },
-            "images": image_urls,
-            # has -> properties table
-            # addresses -> addresses table
-            # sources -> websites table
-            # listed_by -> sellers table
-            # contains -> images table
-        }
+                "url": source_url,
+                "raw_data": {
+                    "html": response_text,
+                    "data": {
+                        "QuidditaEnvironmyent.CurrentClassified": property_data,
+                        "QuidditaEnvironment.CurrentContactData": agency_data,
+                    },
+                },
+                ## additional data
+                "property": {
+                    "property_type": jmespath.search(
+                        "OtherFields.tip_nekretnine_s", property_data
+                    ),
+                    "building_type": jmespath.search(
+                        "OtherFields.tip_objekta_s", property_data
+                    ),
+                    "size_m2": jmespath.search(
+                        "OtherFields.kvadratura_d", property_data
+                    ),
+                    "floor_number": jmespath.search(
+                        "OtherFields.sprat_s", property_data
+                    ),
+                    # COMMENT: sometime it's not available (sprat_od_s)
+                    "total_floors": jmespath.search(
+                        "OtherFields.sprat_od_s", property_data
+                    ),
+                    "rooms": jmespath.search("OtherFields.broj_soba_s", property_data),
+                    # COMMENT: sometime it's not available (stanje_objekta_s)
+                    "property_state": jmespath.search(
+                        "OtherFields.stanje_objekta_s", property_data
+                    ),
+                    # "heating_type": jmespath.search("OtherFields.grejanje_s", property_data),
+                    # "orientation": None,
+                    # Note: Skip for the next iterations
+                    # "has": [
+                    #     # QUESTIONS: how I can find this data?
+                    #     # ANSWER: OtherFields.ostalo_ss
+                    #     {
+                    #         "id": str(uuid.uuid4()),
+                    #         # "feature_type": None,
+                    #         "feature_value": None,
+                    #         "created_at": now,
+                    #     }
+                    # ],
+                },
+                "address": {
+                    "city": jmespath.search("OtherFields.grad_s", property_data),
+                    "municipality": jmespath.search(
+                        "OtherFields.lokacija_s", property_data
+                    ),
+                    "micro_location": jmespath.search(
+                        "OtherFields.mikrolokacija_s", property_data
+                    ),
+                    "latitude": float(geolocation.split(",")[0]),
+                    "longitude": float(geolocation.split(",")[1]),
+                },
+                "source": {
+                    "id": str(uuid.uuid4()),
+                    "name": "Halo Oglasi Nekretnine",
+                    "base_url": "https://www.halooglasi.com",
+                },
+                "seller": {
+                    "source_seller_id": property_data.get("AdvertiserId"),
+                    "name": jmespath.search("Advertiser.DisplayName", agency_data),
+                    # COMMENT: if there's number in register put seller type as 'agency'
+                    "seller_type": (
+                        "agency" if agency_data.get("NumberInRegister") else None
+                    ),
+                    # "license_id": None,
+                    # "tax_id": None,
+                    "primary_phone": phonenumber,
+                    "primary_email": None,
+                    "website": agency_data["WebAddress"],
+                    # "verified": None,
+                    # "rating": None,
+                    # "total_listings": None,
+                    # "active_since": None,
+                },
+                "images": image_urls,
+                # has -> properties table
+                # addresses -> addresses table
+                # sources -> websites table
+                # listed_by -> sellers table
+                # contains -> images table
+            }
+        except Exception as e:
+            db = next(get_db())
+            error_data = Error(
+                url=response.url,
+                error_type="Spider",
+                error_message=str(e),
+                error_traceback=traceback.format_exc(),
+            )
+            db.add(error_data)
+            db.commit()
 
     @json_finder
     def find_property_data(self, response):

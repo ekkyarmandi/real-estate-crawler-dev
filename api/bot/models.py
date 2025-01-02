6cf55b01ada6e2b8e905ae182ac396d7b5a3503f
Ekky Armandi <ekkyarmandi@gmail.com>
2025-01-02 20:07:40 +0700

    fix: allow null at username field

--- api/bot/models.py
+++ api/bot/models.py
@@ -7,15 +7,16 @@ DEFAULT_SETTINGS = {
     "price": "50000-150000",
     "size": "45-120",
     "rooms": "3.0",
+    "is_enabled": True,
 }
 
 
 class User(TimestampedMixin, models.Model):
     id = models.UUIDField(primary_key=True)
-    username = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True, null=True, blank=True)
     chat_id = models.CharField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
-    profile_url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    profile_url = models.CharField(max_length=255, null=True, blank=True)
     settings = models.TextField(default=json.dumps(DEFAULT_SETTINGS))
 
     def __str__(self):

1cd2e08b8f870f2bf8953088c6885f3f0b5cbfc1
Ekky Armandi <ekkyarmandi@gmail.com>
2025-01-20 05:27:53 +0700

    fix: remove sqlalchemy configuration

--- crawler/real_estate_scraper/database.py
+++ crawler/real_estate_scraper/database.py
@@ -2,13 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from decouple import config
 
-engine = create_engine(
-    config("DB_URL"),
-    pool_size=5,  # Maximum number of connections
-    max_overflow=10,  # Maximum number of connections that can be created beyond pool_size
-    pool_timeout=30,  # Seconds to wait before timing out
-    pool_recycle=1800,  # Recycle connections after 1800 seconds
-)
+engine = create_engine(config("DB_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 

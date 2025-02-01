0d054a0fe269b8b59d6af9905e05a9b8835bae35
Ekky Armandi <ekkyarmandi@gmail.com>
2025-02-01 19:38:10 +0700

    fix: crawl.sh missing variable issue

--- crawler/crawl.sh
+++ crawler/crawl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /brixi/crawler
 source /brixi/.venv/bin/activate
-scrapy crawl halooglasi
-scrapy crawl 4zida
-scrapy crawl nekretnine
+scrapy crawl halooglasi -s LOAD_EXISTING_URLS=False
+scrapy crawl 4zida -s LOAD_EXISTING_URLS=False
+scrapy crawl nekretnine -s LOAD_EXISTING_URLS=False

ea6b384a26e3abdd97907c6db5e4070fda94f758
Ekky Armandi <ekkyarmandi@gmail.com>
2025-02-22 19:46:58 +0700

    fix: remove crawl.sh

--- crawler/crawl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /brixi/crawler
-source /brixi/.venv/bin/activate
-scrapy crawl halooglasi -s LOAD_EXISTING_URLS=False
-scrapy crawl 4zida -s LOAD_EXISTING_URLS=False
-scrapy crawl nekretnine -s LOAD_EXISTING_URLS=False

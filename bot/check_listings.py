47f27867367a76b7348bbb74817b9a3c0f647f4a
Ekky Armandi <ekkyarmandi@gmail.com>
2025-02-27 17:13:49 +0700

    feat: add script to check and update listing status for halooglasi.com

--- bot/check_listings.py
+++ bot/check_listings.py
@@ -3,6 +3,7 @@ import requests
 from database import get_db
 from sqlalchemy import text
 from parsel import Selector
+from tqdm import tqdm
 
 # query and fetch all listings
 db = next(get_db())
@@ -20,7 +21,7 @@ urls = db.execute(
 urls = [item[0] for item in urls]
 
 # if the status code is not 200, mark status as 'removed'
-for url in urls:
+for url in tqdm(urls):
     response = requests.get(url)
     if response.status_code == 200:
         selector = Selector(text=response.text)
